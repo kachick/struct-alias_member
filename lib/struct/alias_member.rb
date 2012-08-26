ce7dc30598fcb26b62a6856827a74880031d7713
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-08-27 03:03:10 +0900

    done

--- lib/struct/alias_member.rb
+++ lib/struct/alias_member.rb
@@ -1,17 +1,6 @@
 # Copyright (C) 2012 Kenichi Kamiya
 
-class Struct
-  
-  module Alias_Member
-    
-    VERSION = '0.0.1'.freeze
-    
-    class ArleadyDefinedError < RuntimeError; end
-    
-  end
-
-end
-
+require_relative 'alias_member/version'
 require_relative 'alias_member/classmethods'
 require_relative 'alias_member/instancemethods'
 require_relative 'alias_member/core_ext'
---
+++ test/test_basic.rb
@@ -0,0 +1,46 @@
+$VERBOSE = true
+
+require 'test/declare'
+require_relative '../lib/struct/alias_member'
+
+The Struct.new(:a1, :b2){alias_member :c3, :b2} do |klass|    
+  The klass.members do
+    is [:a1, :b2]
+  end
+  
+  The klass.aliases do
+    is({:c3 => :b2})
+  end
+  
+  The klass.new do |instance|
+    ok {instance.member? :c3}
+    ng {instance.member? :d4}
+    
+    CATCH KeyError do
+      instance[:e5]
+    end
+    
+    The instance.values do
+      is [nil, nil]
+    end
+    
+    The (instance[:c3] = 'OBJ') do
+      same instance[:b2]
+      same instance.b2
+      same instance[:c3]
+      same instance.c3
+    end
+      
+    The instance.values do
+      is [nil, 'OBJ']
+    end
+    
+    The (instance[:a1] = '123') do
+      same instance.a1
+    end
+    
+    The instance.values do
+      is ['123', 'OBJ']
+    end
+  end
+end
